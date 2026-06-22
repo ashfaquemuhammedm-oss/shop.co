6ff7e2e1b4af52670190846c8c081bf2acc8eee8
ITZME85 <fazilfazzndky@gmail.com>
2026-06-22 10:35:13 +0530

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        second: resolve(__dirname, '2ndpage.html'),
+        second: resolve(__dirname, 'casual.html'),
       },
     },
   },
